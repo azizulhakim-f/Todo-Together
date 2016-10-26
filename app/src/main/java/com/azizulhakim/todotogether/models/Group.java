277a2f82d51408fe3549d7928aa6687a57c90ef9
azizulhakim-f <azizulhakim.csedu@gmail.com>
2016-10-26 20:27:44 +0600

    trying to add group... :/

--- app/src/main/java/com/azizulhakim/todotogether/models/Group.java
+++ app/src/main/java/com/azizulhakim/todotogether/models/Group.java
@@ -4,4 +4,12 @@ package com.azizulhakim.todotogether.models;
  * Created by AZIZUL on 10/26/2016.
  */
 public class Group {
+    public String groupid;
+    public String groupname;
+
+
+    public Group() {
+        // Default constructor required for calls to DataSnapshot.getValue(Comment.class)
+    }
+
 }
